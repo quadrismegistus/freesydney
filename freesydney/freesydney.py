31376547f430b1f4156fe223c0b199323a722fd1
Ryan Heuser <ryan.heuser@princeton.edu>
2023-04-07 18:41:37 -0400

    new notebook per successful memory

--- freesydney/freesydney.py
+++ freesydney/freesydney.py
@@ -1,50 +1,75 @@
 from .imports import *
 
-def download_model_orig(model_name:str=DEFAULT_MODEL, force:bool=False):
-    model_fn = model_name+'.bin'
-    model_fnfn = os.path.join(PATH_DATA, model_fn)
-    model_url = MODEL_URLS.get(model_name)
-    if model_url:
-        if force or not os.path.exists(model_fnfn):
-            print(f'Downloading {model_name} to:\n\n{model_fnfn}\n\nIf you already have a file, please a symbolic link to it from that location.\n')
-            download(model_url, model_fnfn)
-    return model_fnfn
 
-def download_tokenizer_orig(force:bool=False):
-    if force or not os.path.exists(TOKENIZER_PATH):
-        download(TOKENIZER_URL,TOKENIZER_PATH)
-    return TOKENIZER_PATH
-
-def convert_model_orig(model_name:str=DEFAULT_MODEL, force:bool=False, exec_name:str='pyllamacpp-convert-gpt4all'):
-    model_fn = download_model_orig(model_name)
-    tokenizer_fn = download_tokenizer_orig()
-    newmodel_fn = os.path.splitext(model_fn)[0] + '.ggml' + os.path.splitext(model_fn)[1]
-    if force or not os.path.exists(newmodel_fn):
-        cmd=f'{exec_name} {model_fn} {tokenizer_fn} {newmodel_fn}'
-        print('>>',cmd)
-        os.system(cmd)
-    return newmodel_fn
-
-def convert_unfiltered_model(): convert_model_orig(DEFAULT_MODEL_UNFILTERED)
-def convert_filtered_model(): convert_model_orig(DEFAULT_MODEL_FILTERED)
-
-
-
-def download_model_converted(model_name:str=DEFAULT_MODEL, force:bool=False):
-    pass
-
-def get_model_converted(model_name:str=DEFAULT_MODEL, force:bool=False, force_convert:bool=False):
-    resfn = ''
-    if not force_convert: resfn = download_model_converted(model_name, force=force)
-    if not resfn: resfn = convert_model_orig(model_name, force=force)
-    return resfn
-
-
-@cache
 def get_model(model_name:str=DEFAULT_MODEL, **kwargs):
     model_fn = get_model_converted(model_name)
-    model = None
-    #with Capturing() as o:
+    ######
+    ## @HACK @FIX @TODO:
+    # force disable stderr output    
+    def donothing(*x,**y): pass
+    sys.stderr.write = donothing
+    ###########
     from pyllamacpp.model import Model
     model = Model(model_fn, **kwargs)
     return model
+
+def Model(): return BaseModel()
+
+def Syd(*args, **kwargs): return BaseModel(*args, **kwargs)
+
+class BaseModel:
+    name=DEFAULT_MODEL
+    system_prompt=''
+    prompt_prefix='> '
+    prompt_suffix='\n'
+    prompt_joiner = '\n\n'
+
+    def __init__(self, **kwargs):
+        self._kwargs=kwargs
+        self.history = []
+        self.prompted = False
+    
+    @cached_property
+    def pyllamacpp(self):
+        return get_model(self.name, **self._kwargs)
+
+    def get_prompt(self, prompt='', with_history=True):
+        if self.system_prompt and not self.history:
+            self.history.append(self.system_prompt)
+
+        if prompt: 
+            prompt = self.prompt_prefix + prompt + self.prompt_suffix
+        if with_history: 
+            prompt = self.prompt_joiner.join(self.history + ([prompt] if prompt else []))
+        return prompt
+
+    def generate(self, prompt, with_history=True, **kwargs):
+        prompt = self.get_prompt(prompt, with_history=with_history)
+        print("## SENDING PROMPT:\n"+prompt+"##########")
+        
+        # send prompt
+        res = self.pyllamacpp.generate(prompt, **kwargs)
+        # find response part
+
+        print("### RESPONSE:"+true_res)
+        true_res = res.split(prompt,1)[-1].strip()
+        self.history.extend([prompt, true_res])
+        self.res = self.prompt_joiner.join(self.history)
+        return true_res
+
+    def sayto(self, prompt, **kwargs):
+        kwargs['with_history'] = True
+        # kwargs['prefix'] = self.prompt_prefix
+        # kwargs['suffix'] = self.prompt_suffix
+        return self.generate(prompt, **kwargs)
+
+
+class Responder(BaseModel): 
+    system_prompt="""
+Below is an instruction that describes a task. Write a response that appropriately completes the request.
+
+> How many letters are there in the English alphabet?
+There 26 letters in the English alphabet.
+
+"""
+
