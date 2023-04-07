ef151a49982cb0334175cb2b72bde1b12106f293
Ryan Heuser <ryan.heuser@gmail.com>
2023-04-07 16:16:24 -0400

    helper functions for getting models

--- freesydney/freesydney.py
+++ freesydney/freesydney.py
@@ -1,13 +1,30 @@
 from .imports import *
 
 
-def download_model(model_name:str=DEFAULT_MODEL, force:bool=False):
+def download_model_orig(model_name:str=DEFAULT_MODEL, force:bool=False):
+    model_fn = model_name+'.bin'
+    model_fnfn = os.path.join(PATH_DATA, model_fn)
     model_url = MODEL_URLS.get(model_name)
     if model_url:
-        model_fn = model_name+'.bin'
-        model_fnfn = os.path.join(PATH_DATA, model_fn)
         if force or not os.path.exists(model_fnfn):
             print(f'Downloading {model_name} to:\n\n{model_fnfn}\n\nIf you already have a file, please a symbolic link to it from that location.\n')
             download(model_url, model_fnfn)
+    return model_fnfn
 
-            
+def download_tokenizer_orig(force:bool=False):
+    if force or not os.path.exists(TOKENIZER_PATH):
+        download(TOKENIZER_URL,TOKENIZER_PATH)
+    return TOKENIZER_PATH
+
+def convert_model_orig(model_name:str=DEFAULT_MODEL, force:bool=False, exec_name:str='pyllamacpp-convert-gpt4all'):
+    model_fn = download_model_orig(model_name)
+    tokenizer_fn = download_tokenizer_orig()
+    newmodel_fn = os.path.splitext(model_fn)[0] + '.ggml' + os.path.splitext(model_fn)[1]
+    if force or not os.path.exists(newmodel_fn):
+        cmd=f'{exec_name} {model_fn} {tokenizer_fn} {newmodel_fn}'
+        print('>>',cmd)
+        os.system(cmd)
+    return newmodel_fn
+
+def download_converted_model(model_name:str=DEFAULT_MODEL, force:bool=False):
+    pass
--- freesydney/imports.py
+++ freesydney/imports.py
@@ -1,6 +1,9 @@
 import os,sys
 
 PATH_HERE = os.path.abspath(os.path.dirname(__file__))
+
+PATH_PYLLAMACPP = os.path.join(PATH_HERE,'pyllamacpp')
+
 PATH_HOME = os.path.expanduser('~')
 PATH_DATA = os.path.abspath(os.path.join(PATH_HOME,'freesydney_data'))
 
@@ -18,5 +21,11 @@ MODEL_URLS = {
     'gpt4all-lora-quantized':'https://the-eye.eu/public/AI/models/nomic-ai/gpt4all/gpt4all-lora-quantized.bin',
 }
 
+CONVERTED_MODEL_URLS = {
+    
+}
+
+TOKENIZER_URL = 'https://huggingface.co/decapoda-research/llama-7b-hf/resolve/main/tokenizer.model'
+TOKENIZER_PATH = os.path.join(PATH_DATA,'tokenizer.model')
 
 from .utils import *
